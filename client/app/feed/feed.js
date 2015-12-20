25d3ba9cc0a3d7e34ea6fdadaa6ba2043bbcb1c4
Brian Thompson <habajca@gmail.com>
2015-12-20 12:48:46 -0800

    Regutted client.

---
+++ client/app/feed/feed.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.feed', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/feed', {
+    templateUrl: 'feed/feed.html',
+    controller: 'FeedCtrl'
+  });
+}])
+
+.controller('FeedCtrl', [function() {
+
+}]);
